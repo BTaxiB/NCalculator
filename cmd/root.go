ce30de756dd540b026aea98b1e5ef29202ecfc6a
BTaxiB <webdevelopermiljan@gmail.com>
2021-12-26 16:00:24 +0100

    feat(NGCalculator): implement basic calculator commands
    Signed-off-by: BTaxiB <webdevelopermiljan@gmail.com>

--- cmd/root.go
+++ cmd/root.go
@@ -2,62 +2,77 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/spf13/cobra"
-	"os"
 
-	"github.com/spf13/viper"
+	"github.com/spf13/cobra"
 )
 
-var cfgFile string
+type Calculator interface {
+	AddNumbers(numbers ...float64) float64
+	SubstractNumbers(numbers ...float64) float64
+	DivideNumbers(dividend float64, divisor float64) (float64, error)
+	MultiplyNumbers(numbers ...float64) float64
+}
+
+type NGCalculator struct {
+	Result float64
+}
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "ngcalculator",
 	Short: "",
-	Long: ``,
-	// Run: func(cmd *cobra.Command, args []string) { },
-}
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
-	cobra.CheckErr(rootCmd.Execute())
+	},
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.ngcalculator.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	calculator := NewCalculator()
 }
 
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
+func NewCalculator() Calculator {
+	return NGCalculator{}
+}
 
-		// Search config in home directory with name ".ngcalculator" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
-		viper.SetConfigName(".ngcalculator")
+func (a NGCalculator) AddNumbers(numbers ...float64) float64 {
+	var result float64
+
+	for _, number := range numbers {
+		result += number
 	}
 
-	viper.AutomaticEnv() // read in environment variables that match
+	return result
+}
 
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+func (a NGCalculator) SubstractNumbers(numbers ...float64) float64 {
+	var result float64
+
+	for _, number := range numbers {
+		result -= number
 	}
+
+	return result
+}
+
+func (a NGCalculator) DivideNumbers(dividend float64, divisor float64) (float64, error) {
+	var result float64
+
+	result = dividend / divisor
+
+	if result < 0 {
+		return 0, fmt.Errorf("failed to divide %f with %f", dividend, divisor)
+	}
+
+	return result, nil
+}
+
+func (a NGCalculator) MultiplyNumbers(numbers ...float64) float64 {
+	var result float64
+
+	for _, number := range numbers {
+		result *= number
+	}
+
+	return result
 }
